b7fb8f50592036665582d9741f3f5710a4e6cc3b
Sainell <xex-x@ya.ru>
2018-04-22 18:25:55 +0300

    Construct

--- Snake/Snake/Snake/Program.cs
+++ Snake/Snake/Snake/Program.cs
@@ -11,17 +11,11 @@ namespace Snake
         static void Main()
         {
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3,'*');           
             p1.Draw();
 
-            Point p2 = new Point();
-            p1.x = 4;
-            p1.y = 5;
-            p1.sym = '$';
-            p1.Draw();
+            Point p2 = new Point(4,5,'#');
+            p2.Draw();
 
 
             Console.ReadLine();
