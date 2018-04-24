4ea0f14696eb19a3ae8ed603b9ca956903c7b285
Sainell <xex-x@ya.ru>
2018-04-24 23:57:38 +0300

    Horizont and Vertical Lines\ Class Figure

--- Snake/Snake/Snake/Program.cs
+++ Snake/Snake/Snake/Program.cs
@@ -12,26 +12,29 @@ namespace Snake
         {
 
             Point p1 = new Point(1, 3,'*');           
-           // p1.Draw();
+            //p1.Draw();
 
             Point p2 = new Point(4,5,'#');
-         //   p2.Draw();
+            //   p2.Draw();
 
-            List<char> charList = new List<char>();
-            charList.Add('+');
-            charList.Add('-');
-            charList.Add('=');
+            //List<char> charList = new List<char>();
+            //charList.Add('+');
+            //charList.Add('-');
+            //charList.Add('=');
 
-            List<Point> pointList = new List<Point>();
-            pointList.Add(p1);
-            pointList.Add(p2);
-            pointList.Add(new Point(5, 5, '&'));
+            //List<Point> pointList = new List<Point>();
+            //pointList.Add(p1);
+            //pointList.Add(p2);
+            //pointList.Add(new Point(5, 5, '&'));
 
-            foreach (Point a in pointList)
-            {
-                a.Draw();
-            }
-
+            //foreach (Point a in pointList)
+            //{
+            //    a.Draw();
+            //}
+           HorizLine Hline = new HorizLine(0, 79, 0, '*');
+            Hline.Draw();
+            VertLine Vline = new VertLine(0, 0, 24, '*');
+            Vline.Draw();
             Console.ReadLine();
 
         }
