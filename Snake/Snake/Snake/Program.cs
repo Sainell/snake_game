7a280bdda2ab31e4d40f79686e4afd28f5dd0cbe
Sainell <xex-x@ya.ru>
2018-04-27 01:55:33 +0300

    control

--- Snake/Snake/Snake/Program.cs
+++ Snake/Snake/Snake/Program.cs
@@ -33,27 +33,25 @@ namespace Snake
 
             Point start = new Point(4, 5, '@');
 
-            Snake snake = new Snake(start, 3, Direction.RIGHT);
+            Snake snake = new Snake(start, 10, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
+
+            
 
 
 
-            Console.ReadLine();
+           
         }
 
        
